9a587bc95237eb0e1e0afa4a277669094f37dd21
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2020-09-19 21:47:31 +0530

    monorepo lerna, netlify, vite

---
+++ typ-game/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Typing />
+</template>
+
+<script>
+import Typing from "./components/Typing.vue";
+
+export default {
+  name: "App",
+  components: {
+    Typing,
+  },
+};
+</script>
---
+++ typ-game/src/components/Typing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="inline-flex">
+    <textarea
+      class="resize border rounded focus:outline-none focus:shadow-outline"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Typing",
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
