84d4e037d2f8e135b4eba4670702b468bc657a5f
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2020-10-10 15:44:11 +0530

    start and finish detection

--- src/components/TypingArea.vue
+++ src/components/TypingArea.vue
@@ -8,18 +8,21 @@
         id="reference"
         class="p-2 bg-gray-100 shadow-md resize-y rounded-b-md focus:outline-none"
         disabled
+        spellcheck="false"
         v-model="referenceText"
       ></textarea>
     </div>
 
     <div class="md:flex-1 m-6 flex flex-col bg-transparent">
-      <di class="bg-gray-200 px-3 py-2 shadow-inner rounded-t-md border-b">
+      <div class="bg-gray-200 px-3 py-2 shadow-inner rounded-t-md border-b">
         <p class="text-gray-700">Practice</p>
-      </di>
+      </div>
       <textarea
         id="practice"
         class="p-2 bg-gray-100 shadow-md resize-y rounded-b-md focus:outline-none"
+        spellcheck="false"
         v-model="practiceText"
+        @input="first_last_Input()"
       ></textarea>
     </div>
   </div>
@@ -31,6 +34,8 @@ export default {
   data() {
     return {
       referenceText: "",
+      started: false,
+      finished: false,
       practiceText: "",
       textareaHeight: Number,
     };
@@ -50,6 +55,23 @@ export default {
         document.getElementById("practice").style.height =
           this.textareaHeight + "px";
       });
+  },
+  methods: {
+    first_last_Input() {
+      if (this.started == false && this.practiceText != "") {
+        this.started = true;
+        console.log(this.started);
+      }
+
+      if (
+        this.started == true &&
+        this.referenceText.length == this.practiceText.length &&
+        this.practiceText.charAt(this.referenceText.length - 1) == "."
+      ) {
+        this.finished = true;
+        console.log(this.finished);
+      }
+    },
   },
 };
 </script>
